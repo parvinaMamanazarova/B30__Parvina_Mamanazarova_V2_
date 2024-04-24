c7f8e86ba24ec980b2ee591b2619c732a7cd2dc5
parvina18 <parvinamamanazarova2@gmail.com>
2024-04-24 10:05:19 +0500

    theoretical ✔️

--- Main.java
+++ Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+import java.util.LinkedList;
+
+public class Main {
+
+
+   public static void main(String[] args) {
+     LinkedList linkedList = new LinkedList();
+     linkedList.add(3);
+     linkedList.add(8);
+     linkedList.add(98);
+
+      System.out.println(linkedList);
+
+
+      LinkedList linkedList1 = new LinkedList();
+       linkedList.get(3);
+       linkedList.get(8);
+       linkedList.get(98);
+       linkedList.get(94);
+       linkedList.get(67);
+       System.out.println("linkedList1.get(3) = " + linkedList1.get(3));
+   }
 }
---
+++ Nazariya.java
@@ -0,0 +1,50 @@
+public class Nazariya {
+
+    //////////////////////////////// 1 /////////////////////////////////////////////
+        /*                          ArrayList
+1) ArrayList Herarcial --> iterabl degan inter ,Kiyin collection degan interface bor . Kiyin List interface bor ,undan kiyin Arraylist .
+
+    Arraylistni ichki strukturasi deteStakchin ni bir bolimi , bir qismi xisoblanadi .
+            DeteStakchin --> bu malumot strukturasi , bular arraylist , yana linklist ....
+
+    Arraylist --> bu bekraundda malumot saqlash uchun dynamic arraydan foydalaniladi .
+    Dynamic array --> grow()(methodi) size osib boradigon method ishlatiladi.
+
+            Arraylist --> arraydan malumotlarni ketma ket joy oladi , biz kop uzunlik kiritib qoysak kiyin AutuoMemoryError chiqaradi . Error beradi .
+
+            ------------------------------------------------------------------------------------
+                                        LinkedList
+    LinkedList --> Node degan narsa saqlaydi . Node bu bitta class , u ozidan kiyingi elementlarni qayerda turganini biladi .
+
+
+    ArrayList va LinkedList
+    Ikkalasini farlari --> Arraylist malumotlarni arrayga saqlaydi , LinkedList esa Node degan classda saqlaydi .
+
+/////////////////////////////////// 2 ///////////////////////////////////////////////
+2) LinkedHashMap --> u xam malumotlarni Datatrakchinda saqlaydi HashMap kabi , u (Array)Node tipida  saqlaydi .
+
+/////////////////////////////////// 3 //////////////////////////////////////////////
+                      Genery
+3) Genary --> mavhum tip yoki parametayzet type(parametrlashtirilgan tip) biz unga nima bersak oshanga qarab azgaraveradi.
+
+    Generyc --> faqatgina compile time da kompilyatsiya bolaydigon typeseftini beradi
+    Typesefty --> biz yozayotgan metodlarimizni faqatgina belgilangan malumotlarni gina ishlata olishi  bomasa ishlata olmaydi .
+
+    Ganary -->  bizni kodimizni yoziwga osonlashtirib beradi , biz uni nima tip da ekanligini ayitib otirmaymiz , ozi bilib oladi .
+
+
+//////////////////////////////////// 4 //////////////////////////////////////////////
+                    Collection
+4) Collection heirarcy --> 1. iterebl
+			2.Collection
+
+1.List                 2.Queue              3.Set
+
+1.ArrayList           1.PreorityQueue        1.Set
+2.LinkList            2.Deque                2. TreSet
+3.Vektor
+
+}
+
+*/
+}
